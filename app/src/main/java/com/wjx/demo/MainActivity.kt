bcdc50a939fea1487d248b7cdb9d6e5191f2c1c5
asdf123ttt <2091243825@QQ.com>
2021-09-08 16:51:33 +0800

    wjx

--- app/src/main/java/com/wjx/demo/MainActivity.kt
+++ app/src/main/java/com/wjx/demo/MainActivity.kt
@@ -2,10 +2,14 @@ package com.wjx.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //王家兴
+        var a=1
+
     }
 }
