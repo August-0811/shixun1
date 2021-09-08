07f22001470d62aefe412dedf0f02d186db197e9
asdf123ttt <2091243825@QQ.com>
2021-09-08 18:29:49 +0800

    qwe

--- app/src/main/java/com/wjx/demo/MainActivity.kt
+++ app/src/main/java/com/wjx/demo/MainActivity.kt
@@ -8,8 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //王家兴
-        var a=1
 
     }
 }
